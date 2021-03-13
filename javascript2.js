4c10a7f26646dc0cb95b3642476ca91e2bcf677b
EC2 Default User <ec2-user@ip-172-31-10-198.ap-northeast-1.compute.internal>
2021-03-13 00:52:44 +0000

    javascript課題２提出

---
+++ javascript2.js
@@ -0,0 +1,37 @@
+let nowTotal = 0; //現在の合計値
+let nowInput = ""; //現在の入力値
+let operator = "+"; //合計と入力値の演算子
+let judgment = 1; //直前に入力したものの判定 0:数字 1:演算子
+let calculate = ""; //計算式の作成
+
+function input_number(number){
+	judgment = 0;
+	nowInput += number;
+	document.calculate.result.value = nowInput;
+}
+
+function input_operator(symbol){
+	if(judgment === 0){
+		judgment = 1;
+		calculate = nowTotal + operator + nowInput;
+		nowTotal = eval(calculate);
+		nowInput = "";
+		document.calculate.result.value = nowTotal;
+	}
+	if(symbol === "="){
+		nowTotal = 0;
+		operator = "+";
+	}else{
+		operator = symbol;
+	}
+}
+
+function input_clear(){
+	nowTotal = 0;
+	nowInput = "";
+	operator = "+";
+	judgment = 1;
+	calculate = "";
+	document.calculate.result.value = 0;
+}
+    
